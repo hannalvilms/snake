1575c4fb6b21eaa936f515bcb7092a01f11aac90
Hanna-Liisa Vilms <hanna-liisa.vilms@khk.ee>
2020-03-10 12:00:09 +0200

    Updates

---
+++ precache-manifest.502b7100f01a6069501aef48f891728a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9613ca3526be1309bca10cafa31f40",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "9606cb05dcde9a4a7875",
+    "url": "/snake/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "b1f7e7e705f75ae75cdc",
+    "url": "/snake/static/css/main.67008822.chunk.css"
+  },
+  {
+    "revision": "9606cb05dcde9a4a7875",
+    "url": "/snake/static/js/2.3853058c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/snake/static/js/2.3853058c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1f7e7e705f75ae75cdc",
+    "url": "/snake/static/js/main.48fcfb9a.chunk.js"
+  },
+  {
+    "revision": "c771c376c8ee08efe756",
+    "url": "/snake/static/js/runtime-main.a70c655e.js"
+  }
+]);
